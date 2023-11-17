98e5a056c7586d8b9e03963917466fdc026a4a2a
ze <jr.carvalho@usp.br>
2023-11-17 15:59:22 -0300

    melhoras no script

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# CEN0336_Seminario
+CEN0336_Seminario
+    Baixa as planilhas de dados da web e localiza os dados de precipitacao.
--- seminario.py
+++ seminario.py
@@ -1,32 +1,36 @@
+#!/usr/bin/env python3
+
 import requests as rq
+import os.path as os
+import sys
 
-# URL base do site
-url_das_planilhas = "http://www.leb.esalq.usp.br/exceldados/{}{}{}.xls"
+#URL base do site
+url_planilhas = "http://www.leb.esalq.usp.br/exceldados/{}{}.xls"
 
-# Diretório onde você deseja salvar as planilhas
-diretorio_de_download = r"C:/Users/willb/Downloads/Dados/"
+# Diretorio atual para salvar as planilhas
+diretorio_de_download = os.dirname(sys.argv[0])
 
-# Loop de 1917 a 2023
+# Loop de 1917 a 2023 e definicao dos links de download
 for ano in range(1917, 2018):
     if ano < 2000:
-        url = url_das_planilhas.format("ROB", str(ano)[2:], "")
+        url = url_planilhas.format("ROB", str(ano)[2:])
     else:
-        url = url_das_planilhas.format("DCE", str(ano), "")
+        url = url_planilhas.format("DCE", str(ano))
 
-    # Faça a solicitação para o servidor
+    #Faca a solicitacao para o servidor
     solicitacao_dos_dados = rq.get(url)
 
-    # Verifique se a solicitação foi bem-sucedida
+    #Verifique se a solicitacao foi bem-sucedida
     if solicitacao_dos_dados.status_code == 200:
-        # Nomeie o arquivo de acordo com o ano e a extensão .xls
+        # Nomeie o arquivo de acordo com o ano e a extensao .xls
         nome = f"{diretorio_de_download}{ano}.xls"
 
-        # Salve a planilha no diretório especificado
+        # Salve a planilha no diretorio especificado
         with open(nome, "wb") as file:
             file.write(solicitacao_dos_dados.content)
         print(f"Planilha do ano {ano} baixada com sucesso!")
 
     else:
-        print(f"Não foi possível baixar a planilha do ano {ano}.")
+        print(f"Nao foi possivel baixar a planilha do ano {ano}.")
 
-print("Download de planilhas concluído.")
+print("Download de planilhas concluido.")
