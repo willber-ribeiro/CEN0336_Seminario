1f58c710981820984e3194f766fc6d1d678cc9f5
ze <jr.carvalho@usp.br>
2023-12-01 00:08:37 -0300

    Precipi total mensal e anual

--- gera_valores_estatisticos.py
+++ gera_valores_estatisticos.py
@@ -17,20 +17,20 @@ dados_filtrados_mes = df[(df['ANO'] == ano_desejado) & (df['MES'].str.strip().st
 dados_filtrados_ano = df[(df['ANO'] == ano_desejado)]
 
 #Calcular os valores de média mensal e anual
-media_precipitacao_mes = dados_filtrados_mes['PRECIPI-'].mean()
+total_precipitacao_mes = dados_filtrados_mes['PRECIPI-'].sum()
 precipitacao_max_mes = dados_filtrados_mes['PRECIPI-'].max()
 #precipitacao_min_mes = dados_filtrados_mes['PRECIPI-'].min()
 
-media_precipitacao_ano = dados_filtrados_ano['PRECIPI-'].mean()
+total_precipitacao_ano = dados_filtrados_ano['PRECIPI-'].sum()
 precipitacao_max_ano = dados_filtrados_ano['PRECIPI-'].max()
 #precipitacao_min_ano = dados_filtrados_ano['PRECIPI-'].min()
 
 # Exibir resultados
-print(f'A média mensal de precipitação para {mes_desejado}-{ano_desejado} é: {media_precipitacao_mes:.2f} mm')
+print(f'O total de precipitação para {mes_desejado}-{ano_desejado} é: {total_precipitacao_mes:.2f} mm')
 print(f'A precipitação máxima ocorrida em {mes_desejado}-{ano_desejado} é: {precipitacao_max_mes:.2f} mm')
 #print(f'A precipitação mínima ocorrida em {mes_desejado}-{ano_desejado} é: {precipitacao_min_mes:.2f} mm')
 
-print(f'A média anual de precipitação para {ano_desejado} é: {media_precipitacao_ano:.2f} mm')
+print(f'O total anual de precipitação para {ano_desejado} é: {total_precipitacao_ano:.2f} mm')
 print(f'A precipitação máxima ocorrida em {ano_desejado} é: {precipitacao_max_ano:.2f} mm')
 #print(f'A precipitação mínima ocorrida em {ano_desejado} é: {precipitacao_min_ano:.2f} mm')
     # De maneira geral, a precipitação mínima em um mês ou ano sempre vai ser zero, então não acho que seja necessário o script verificar isso. De qualquer forma eu deixei o código aqui, é só tirar o # que o script volta a incluir a precipitação mínima.
