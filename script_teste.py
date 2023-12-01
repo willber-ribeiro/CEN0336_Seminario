c55c54f2d04e7065b606ca9f97dc18bafb5d8998
ze <jr.carvalho@usp.br>
2023-12-01 15:56:33 -0300

    Limpei o script_teste.py

--- script_teste.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-import pandas as pd
-
-df_1917 = pd.read_excel('1917.xls')
-df_1917.columns = df_1917.iloc[0]
-print(df_1917.loc[pd.to_numeric(df_1917['No'], errors = 'coerce').notnull(),['No','ANO     ','DIA     ','MES     ','PRECIPI-']])
